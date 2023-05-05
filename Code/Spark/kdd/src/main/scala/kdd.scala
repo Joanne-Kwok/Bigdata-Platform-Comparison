0a160995556f75b83585a6fbe2d0a5a6514ccdac
Xueying Guo <48356714+Joanne-Kwok@users.noreply.github.com>
2023-05-05 10:58:29 +0930

    Update kdd.scala

--- Code/Spark/kdd/src/main/scala/kdd.scala
+++ Code/Spark/kdd/src/main/scala/kdd.scala
@@ -15,9 +15,6 @@ object SVM {
 val df = new SimpleDateFormat("yyyy-dd-MM HH:mm:ss:SSSS")
 
      def main(args: Array[String]): Unit = {
-    //val conf = new SparkConf().setAppName("SVMWithSGDExample").setMaster("local")
-    //val conf = new SparkConf().setAppName("TestSVM").setMaster("local")
-    //val conf = new SparkConf().setAppName("TestSVM").setMaster("local")
     val conf = new SparkConf().setAppName("kdd")
     val sc = new SparkContext(conf)
 
@@ -25,8 +22,6 @@ println("Job Start! Current Time is: " + df.format(System.currentTimeMillis()))
     val jobstarttime = System.currentTimeMillis()
     
     println("------------------------Read File-------------------------")
-    //val traindata = sc.textFile("/home/hadoop/Desktop/svm/20KDDTrain.txt")   
-    //val testdata = sc.textFile("/home/hadoop/Desktop/svm/20KDDTest.txt")
     val traindata = sc.textFile("/user/ubuntu/Hadoop/nb/kdd/train/KDDTrain.txt")   
     val testdata = sc.textFile("/user/ubuntu/Hadoop/nb/kdd/test/KDDTest.txt")
 
@@ -35,13 +30,10 @@ println("Transform Data! Current Time is: " + df.format(System.currentTimeMillis
     println("-------------------Read file Complete -> Set LabelPoint----------------------")
     val training = traindata.map { line =>
       val parts = line.split(',')
-      //val parts2 = line.split(',').filter(_)
         LabeledPoint(
             if (parts(41)=="normal.") 0.toDouble
             else 1.toDouble,
-// parts(47).toDouble,
 Vectors.dense(parts(0).toDouble,parts(1).toDouble,parts(2).toDouble,parts(3).toDouble,parts(4).toDouble,parts(5).toDouble,parts(6).toDouble,parts(7).toDouble,parts(8).toDouble,parts(9).toDouble,parts(10).toDouble,parts(11).toDouble,parts(12).toDouble,parts(13).toDouble,parts(14).toDouble,parts(15).toDouble,parts(16).toDouble,parts(17).toDouble,parts(18).toDouble,parts(19).toDouble,parts(20).toDouble,parts(21).toDouble,parts(22).toDouble,parts(23).toDouble,parts(24).toDouble,parts(25).toDouble,parts(26).toDouble,parts(27).toDouble,parts(28).toDouble,parts(29).toDouble,parts(30).toDouble,parts(31).toDouble,parts(32).toDouble,parts(33).toDouble,parts(34).toDouble,parts(35).toDouble,parts(36).toDouble,parts(37).toDouble,parts(38).toDouble,parts(39).toDouble,parts(40).toDouble)
-            //Vectors.dense(parts.map(_.toDouble))
         )
        }.cache()
 
@@ -70,7 +62,6 @@ Vectors.dense(parts(0).toDouble,parts(1).toDouble,parts(2).toDouble,parts(3).toD
         LabeledPoint(
             if (parts(41)=="normal.") 0.toDouble
             else 1.toDouble, 
-//            parts(41).toDouble, 
 Vectors.dense(parts(0).toDouble,parts(1).toDouble,parts(2).toDouble,parts(3).toDouble,parts(4).toDouble,parts(5).toDouble,parts(6).toDouble,parts(7).toDouble,parts(8).toDouble,parts(9).toDouble,parts(10).toDouble,parts(11).toDouble,parts(12).toDouble,parts(13).toDouble,parts(14).toDouble,parts(15).toDouble,parts(16).toDouble,parts(17).toDouble,parts(18).toDouble,parts(19).toDouble,parts(20).toDouble,parts(21).toDouble,parts(22).toDouble,parts(23).toDouble,parts(24).toDouble,parts(25).toDouble,parts(26).toDouble,parts(27).toDouble,parts(28).toDouble,parts(29).toDouble,parts(30).toDouble,parts(31).toDouble,parts(32).toDouble,parts(33).toDouble,parts(34).toDouble,parts(35).toDouble,parts(36).toDouble,parts(37).toDouble,parts(38).toDouble,parts(39).toDouble,parts(40).toDouble)
         )
        }
@@ -139,7 +130,6 @@ println("Prediction End! Current Time is: " + df.format(System.currentTimeMillis
     val R = tp.toDouble/(tp+fn).toDouble // recall rate
     val f1 = 2.toDouble/(1.toDouble/precision.toDouble + 1.toDouble/R.toDouble).toDouble
     val dr = tp.toDouble / (tp+fp).toDouble
-    //scoreAndLabels.foreach(println)
 
 
     println("Job End! Current Time is: " + df.format(System.currentTimeMillis()))
@@ -171,7 +161,6 @@ println("Prediction End! Current Time is: " + df.format(System.currentTimeMillis
 
     // Get evaluation metrics.
     val metrics = new BinaryClassificationMetrics(scoreAndLabels)
-    //val metrics = new MulticlassMetrics(scoreAndLabels)
     val auROC = metrics.areaUnderROC()
 
     println("Area under ROC = " + auROC)
